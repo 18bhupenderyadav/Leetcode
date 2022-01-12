99386102287d516f2d4ccc0483e0d9dc30b5fd68
18bhupenderyadav <72654756+18bhupenderyadav@users.noreply.github.com>
2022-01-12 20:02:53 +0530

    Time: 1322 ms (5.04%), Space: 190.3 MB (5.04%) - LeetHub

---
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,101 @@
+
+class Solution {
+public:
+    const int inf=1e6;
+    map<int,vector<int>> mp;
+    vector<int> tree;
+    int nm;
+    int find(int node,int node_low,int node_high,int low,int high)
+    {
+        if(high<node_low || low>node_high)
+            return inf;
+        
+        if(node_low>=low && node_high<=high)
+            return tree[node];
+        
+        int mid=(node_low+node_high)/2;
+        return min(find(2*node,node_low,mid,low,high),find(2*node+1,mid+1,node_high,low,high));
+    }
+    
+    void sege(vector<int>& ar)
+    {
+        int n=ar.size();
+        
+        while((n&(n-1))!=0)
+        {
+            n++;
+        }
+        
+        tree.resize(2*n,inf);
+        
+        for(int i=0;i<ar.size();i++)
+        {
+            tree[i+n]=ar[i];
+        }
+        
+        for(int i=n-1;i>0;i--)
+        {
+            tree[i]=min(tree[2*i],tree[2*i+1]);
+        }
+    }
+    
+    int dp(vector<int>& ar,int st,int end,int lmin)
+    {
+        if(st==end)
+            return ar[st]-lmin;
+        if(st>end)
+            return 0;
+    
+        int ans=0,mn=INT_MAX,ls=st;
+
+        mn=find(1,0,nm-1,st,end);
+        
+        ans+=mn-lmin;
+        
+        for(int i:mp[mn])
+        {
+            if(i>=st && i<=end)
+            {
+                ans+=dp(ar,ls,i-1,mn);
+                ls=i+1;
+            }
+        }
+        
+        
+        ans+=dp(ar,ls,end,mn);
+        
+        return ans;
+    }
+    int minNumberOperations(vector<int>& ar) 
+    {
+        int n=ar.size();
+        
+        for(int i=0;i<n;i++)
+            mp[ar[i]].push_back(i);
+        
+        sege(ar);
+        
+        while((n&(n-1))!=0)
+            n++;
+        
+        nm=n;
+        
+        int mn=INT_MAX,ls=0;
+
+        mn=find(1,0,nm-1,0,n-1);
+        
+        int ans=mn;
+        
+        for(int i:mp[mn])
+        {
+            ans+=dp(ar,ls,i-1,mn);
+            ls=i+1;
+        }
+
+        ans+=dp(ar,ls,ar.size()-1,mn);
+
+        return ans;
+    }
+};
+
+// 1 2 3 2 1
