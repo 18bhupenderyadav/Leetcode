dffd6d9d674c66bcf6e10f4bf94836f0d72028c8
18bhupenderyadav <72654756+18bhupenderyadav@users.noreply.github.com>
2022-01-27 17:03:58 +0530

    Time: 48 ms (41.35%), Space: 13.8 MB (99.73%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -4,10 +4,15 @@ class Solution:
         if(n==0):
             return 0
         
+        memo=[0 for i in range(n+1)]
         def f(n):
             if(n<=2):
                 return 1
-            return f(n-1)+f(n-2)
+            
+            if n not in memo:
+                memo[n]=f(n-1)+f(n-2)
+            
+            return memo[n]
         
         return f(n)
         
