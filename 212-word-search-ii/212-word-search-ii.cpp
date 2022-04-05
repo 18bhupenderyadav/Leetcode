c790d575c2cea10824620f8c83f0f8a81a4b625e
18bhupenderyadav <72654756+18bhupenderyadav@users.noreply.github.com>
2022-04-05 18:03:08 +0530

    Time: 1880 ms (5.02%), Space: 13.1 MB (49.50%) - LeetHub

---
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -0,0 +1,82 @@
+class Solution {
+public:
+
+    
+    void dfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& br,vector<int>& isEnd ,string& s,int node,vector<vector<int>>& nxt,unordered_set<string>& ans)
+    {
+        if(isEnd[node])
+            ans.insert(s);
+        if(i<0 || j<0 || i>=br.size() || j>=br[i].size())
+            return;
+        if(vis[i][j])
+            return;
+        
+        int cur = br[i][j]-'a';
+        if(nxt[node][cur] == 0)
+            return;
+        
+        vis[i][j]=1;
+        s+= br[i][j];
+        
+        
+        int new_node = nxt[node][cur];
+        
+        dfs(i+1,j,vis,br,isEnd,s,new_node,nxt,ans);
+        dfs(i-1,j,vis,br,isEnd,s,new_node,nxt,ans);
+        dfs(i,j+1,vis,br,isEnd,s,new_node,nxt,ans);
+        dfs(i,j-1,vis,br,isEnd,s,new_node,nxt,ans);
+        
+        s.pop_back();
+        vis[i][j]=0;
+        return;
+        
+    }
+    
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) 
+    {
+        // Build a trie
+        
+        vector<vector<int>> nxt(1,vector<int>(26,0));
+        vector<int> isEnd(1,0);
+        
+        int n = 0;
+        
+        for(int i=0;i<words.size();i++)
+        {
+            int node = 0;
+            for(int j=0;j<words[i].size();j++)
+            {
+                int cur = words[i][j]-'a';
+                if(nxt[node][cur] == 0)
+                {
+                    n++;
+                    nxt[node][cur] = n;
+                    nxt.push_back(vector<int>(26,0));
+                    isEnd.push_back(0);
+                }
+                node = nxt[node][cur];
+            }
+            isEnd[node] = 1;
+        }
+        
+        // DFS using the trie
+        unordered_set<string> ans;
+        vector<vector<int>> vis(board.size(),vector<int>(board[0].size(),0));
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[i].size();j++)
+            {
+                
+                string s = "";
+                dfs(i,j,vis,board,isEnd,s,0,nxt,ans);
+            }
+        }
+        vector<string> sol;
+        
+        for(auto i:ans)
+            sol.push_back(i);
+        
+        return sol;
+        
+    }
+};
