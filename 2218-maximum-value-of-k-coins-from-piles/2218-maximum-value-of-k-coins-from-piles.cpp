69cccea7cee58711085e6df09d641b7f5c379fec
18bhupenderyadav <72654756+18bhupenderyadav@users.noreply.github.com>
2022-04-28 20:48:16 +0530

    Time: 306 ms (72.69%), Space: 19.6 MB (29.54%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int memo[1005][2005];
+    int memo[1001][2001];
     int dp(int pos,int k,vector<vector<int>>& ar)
     {
         if(k<0)
@@ -14,7 +14,7 @@ public:
             return memo[pos][k];
         
         int ans = dp(pos+1,k,ar),sum=0;
-        for(int i=0;i<ar[pos].size();i++)
+        for(int i=0;i<min(k,(int)ar[pos].size());i++)
         {
             sum+=ar[pos][i];
             ans=max(ans,sum+dp(pos+1,k-i-1,ar));
