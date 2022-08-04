10591dbfef94578e03416eac1d791fa3917fbfa9
18bhupenderyadav <72654756+18bhupenderyadav@users.noreply.github.com>
2022-08-04 17:58:15 +0530

    Time: 928 ms (23.86%), Space: 129.3 MB (56.65%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void add(vector<int>& ans, vector<int>& temp)
+    {
+        set<int> s;
+        
+        for(int i:ans)
+            s.insert(i);
+        
+        for(int i: temp)
+            if(s.find(i) == s.end())
+                ans.push_back(i);
+        
+        return;
+    }
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) 
+    {
+        vector<vector<int>> ans(n),adj(n);
+        
+        queue<int> q;
+        
+        vector<int> in_deg(n,0);
+        
+        for(auto i:edges)
+        {
+            in_deg[i[1]]++;
+            
+            adj[i[0]].push_back(i[1]);
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(in_deg[i]==0)
+                q.push(i);
+        }
+        
+        while(q.size())
+        {
+            int top = q.front();
+            q.pop();
+            
+            for(auto i:adj[top])
+            {
+                in_deg[i]--;
+                
+                if(in_deg[i]==0)
+                {
+                    q.push(i);
+                }
+                
+                add(ans[i],ans[top]);
+                ans[i].push_back(top);
+            }
+        }
+        
+        for(auto& i:ans)
+            sort(i.begin(),i.end());
+        
+        return ans;
+    }
+};
